c5658181303269b2643cdae46a5f3c2d0c6eaedb
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-05-10 19:05:35 -0300

    Basic Architecture React

---
+++ projeto14-boot_store-front/src/App.jsx
@@ -0,0 +1,3 @@
+export default () => {
+	return <h1>Já podemos começar a codar!</h1>;
+};
---
+++ projeto14-boot_store-front/src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+export default () => {
+    return (
+        <>
+            <h1>HOME</h1>
+        </>
+    );
+};
