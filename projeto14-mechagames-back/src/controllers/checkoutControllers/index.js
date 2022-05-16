51e43b631c623d528beb27e191ae844023934caf
Thales Gomes Targino <thalestargino@gmail.com>
2022-05-16 13:18:18 -0300

    feat: SendGrid email confirmation

--- projeto14-mechagames-back/src/controllers/checkoutControllers/index.js
+++ projeto14-mechagames-back/src/controllers/checkoutControllers/index.js
@@ -1,28 +1,18 @@
 import savePurchase from '../../services/purchaseServices';
 
 const finishPurchase = async (req, res, next) => {
-    try {
-        const token = (
-            req
-                .headers
-                .authorization
-                .replace('Bearer', '')
-                .trim()
-        );
-        if (typeof token === 'string') {
-            const { products } = req.body;
-            await savePurchase(products, token);
-            res.sendStatus(204);
-        } else {
-            throw new CustomError(
-                403,
-                'Forbidden',
-                "Client didn't send token",
-            );
-        }
-    } catch (e) {
-        next(e);
-    }
+	try {
+		const token = req.headers.authorization.replace('Bearer', '').trim();
+		if (typeof token === 'string') {
+			const { products } = req.body;
+			await savePurchase(products, token);
+			res.sendStatus(204);
+		} else {
+			throw new CustomError(403, 'Forbidden', "Client didn't send token");
+		}
+	} catch (e) {
+		next(e);
+	}
 };
 
 export default finishPurchase;
