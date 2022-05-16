62e2a9533088dd7e8a56f977eac5c168ccd5408f
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-05-15 22:23:51 -0300

    chart functions

---
+++ projeto14-mechagames-front/src/components/mainComponents/Chart/styles.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const StyledContainer = styled.div`
+    position: fixed;
+    background: black;
+    z-index: 2;
+    height: 100vh;
+    width: 100vw;
+`; 
+
+export const StyledAside = styled.aside`
+    position: fixed;
+    background: white;
+    z-index: 3;
+    height: 100vh;
+    width: 30vw;
+
+`;
--- projeto14-mechagames-front/src/components/mainComponents/Header/index.jsx
+++ projeto14-mechagames-front/src/components/mainComponents/Header/index.jsx
@@ -1,31 +1,36 @@
-import { useNavigate } from 'react-router-dom';
-import { BiSearch } from 'react-icons/bi';
-import { GiShoppingCart } from 'react-icons/gi';
-import { StyledHeader, HeaderButton } from './styles.jsx';
+import { useState } from "react";
+import {useNavigate} from "react-router-dom";
+import {StyledHeader, HeaderButton} from "./styles.jsx";
+import {BiSearch} from 'react-icons/bi';
+import {GiShoppingCart} from 'react-icons/gi';
 import MechaGamesLogo from '../../../assets/images/mecha-games-logo.svg';
 import Controllers from '../../../assets/images/controllers.svg';
+import Chart from "../Chart/index.jsx";
 
 export default () => {
     const navigate = useNavigate();
+    const [flag, setFlag] = useState(false);
     return (
-        <StyledHeader>
-            <div className="brand">
-                <img
-                    className="logo"
-                    src={MechaGamesLogo}
-                    alt="mecha-games-logo"
-                    onClick={() => navigate('/')}
-                />
-                <h3>MECHA GAMES</h3>
-            </div>
-            <div className="icons-header">
-                <BiSearch className="magnifying-glass" />
-                <GiShoppingCart className="cart" />
-                <HeaderButton onClick={() => navigate('/sign-in')}>
-					login
-                </HeaderButton>
-            </div>
-            <img className="fixed_image" src={Controllers} alt="controllers" />
-        </StyledHeader>
+        <>  
+            {flag && <Chart/>}
+            <StyledHeader>
+                <div className="brand">
+                    <img
+                        className="logo"
+                        src={MechaGamesLogo}
+                        alt="mecha-games-logo"
+                    />
+                    <h3>MECHA GAMES</h3>
+                </div>
+                <div className="icons-header">
+                    <BiSearch className="magnifying-glass"/>
+                    <GiShoppingCart className="cart" onClick={() => setFlag(true)}/>
+                    <HeaderButton onClick={() => navigate('/sign-in')}>
+                        login
+                    </HeaderButton>
+                </div>
+                <img className="fixed_image" src={Controllers} alt="controllers" />
+            </StyledHeader>
+        </>
     );
 };
